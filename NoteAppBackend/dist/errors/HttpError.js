aa3b4b9539d0de0878cad10b855554666987ce6e
Luka <lkordic@hotmail.com>
2024-04-12 13:32:15 +0200

    add dist to .gitignore

--- NoteAppBackend/dist/errors/HttpError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class HttpError extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        super.name = "HttpError";
-        this.statusCode = statusCode;
-    }
-}
